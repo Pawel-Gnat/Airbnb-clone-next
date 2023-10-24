3817f2066d6c3965e1ca0006cc0f2be9e816b517
Paweł <pawel_gnat@o2.pl>
2023-10-24 11:06:50 +0200

    Modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
+import Modal from './components/modals/Modal'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -14,9 +15,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang='en'>
 			<body className={font.className}>
-        <Navbar/>
-        {children}
-        </body>
+				<Modal
+					actionLabel='Submit'
+					secondaryActionLabel='Cancel'
+					isOpen
+					title='Hello world'
+				/>
+				<Navbar />
+				{children}
+			</body>
 		</html>
 	)
 }
