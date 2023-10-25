df570f93601cd5a7192fe482666269407dae2f3d
Paweł <pawel_gnat@o2.pl>
2023-10-25 11:18:30 +0200

    feat: categories list

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -2,10 +2,8 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 
 const Logo = () => {
-	const router = useRouter()
 	return (
 		<Link href='/'>
 			<Image
