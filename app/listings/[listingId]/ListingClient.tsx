f3e20159b73fe4a5f4a316d75f7bece49f945e8e
Paweł <pawel_gnat@o2.pl>
2023-10-29 16:29:50 +0100

    feat: Calendar component

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,24 +1,94 @@
 'use client'
 
-import { useMemo } from 'react'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { differenceInCalendarDays, eachDayOfInterval } from 'date-fns'
 import { Listing, Reservation } from '@prisma/client'
+import { Range } from 'react-date-range'
 
+import useLoginModal from '@/app/hooks/useLoginModal'
 import { SafeUser } from '@/app/types'
 import { categories } from '@/app/components/navbar/Categories'
 import Container from '@/app/components/Container'
 import ListingHead from '@/app/components/listings/ListingHead'
 import ListingInfo from '@/app/components/listings/ListingInfo'
+import ListingReservation from '@/app/components/listings/ListingReservation'
 
+const initialDateRange = {
+	startDate: new Date(),
+	endDate: new Date(),
+	key: 'selection',
+}
 interface ListingClientProps {
 	reservations?: Reservation[]
-	// listing: Listing & {
-	// 	user: SafeUser
-	// }
 	listing: Listing
 	currentUser?: SafeUser | null
 }
 
-const ListingClient: React.FC<ListingClientProps> = ({ reservations, listing, currentUser }) => {
+const ListingClient: React.FC<ListingClientProps> = ({ reservations = [], listing, currentUser }) => {
+	const loginModal = useLoginModal()
+	const router = useRouter()
+
+	const disabledDates = useMemo(() => {
+		let dates: Date[] = []
+
+		reservations.forEach(reservation => {
+			const range = eachDayOfInterval({
+				start: new Date(reservation.startDate),
+				end: new Date(reservation.endDate),
+			})
+
+			dates = [...dates, ...range]
+		})
+
+		return dates
+	}, [reservations])
+
+	const [isLoading, setIsLoading] = useState(false)
+	const [totalPrice, setTotalPrice] = useState(listing.price)
+	const [dateRange, setDateRange] = useState<Range>(initialDateRange)
+
+	const onCreateReservation = useCallback(() => {
+		if (!currentUser) {
+			return loginModal.onOpen()
+		}
+		setIsLoading(true)
+
+		axios
+			.post('/api/reservations', {
+				totalPrice,
+				startDate: dateRange.startDate,
+				endDate: dateRange.endDate,
+				listingId: listing?.id,
+			})
+			.then(() => {
+				toast.success('Listing reserved!')
+				setDateRange(initialDateRange)
+
+				router.refresh()
+			})
+			.catch(() => {
+				toast.error('Something went wrong.')
+			})
+			.finally(() => {
+				setIsLoading(false)
+			})
+	}, [currentUser, dateRange, loginModal, totalPrice, listing?.id, router])
+
+	useEffect(() => {
+		if (dateRange.startDate && dateRange.endDate) {
+			const dayCount = differenceInCalendarDays(dateRange.endDate, dateRange.startDate)
+
+			if (dayCount && listing.price) {
+				setTotalPrice(dayCount * listing.price)
+			} else {
+				setTotalPrice(listing.price)
+			}
+		}
+	}, [dateRange, listing.price])
+
 	const category = useMemo(() => {
 		return categories.find(item => item.label === listing.category)
 	}, [listing.category])
@@ -36,7 +106,6 @@ const ListingClient: React.FC<ListingClientProps> = ({ reservations, listing, cu
 					/>
 					<div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
 						<ListingInfo
-							// user={listing.user}
 							user={currentUser}
 							category={category}
 							description={listing.description}
@@ -45,6 +114,17 @@ const ListingClient: React.FC<ListingClientProps> = ({ reservations, listing, cu
 							bathroomCount={listing.bathroomCount}
 							locationValue={listing.locationValue}
 						/>
+						<div className='order-first mb-10 md:order-last md:col-span-3'>
+							<ListingReservation
+								price={listing.price}
+								totalPrice={totalPrice}
+								onChangeDate={value => setDateRange(value)}
+								dateRange={dateRange}
+								onSubmit={onCreateReservation}
+								disabled={isLoading}
+								disabledDates={disabledDates}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
